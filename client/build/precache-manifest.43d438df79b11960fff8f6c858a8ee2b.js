65fc821ac3caa6f90df124060c66959d860b44f6
Abhijeet Rajak <rajakabhijeet6@gmail.com>
2020-12-01 23:59:17 +0530

    Done

---
+++ client/build/precache-manifest.43d438df79b11960fff8f6c858a8ee2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c02afb953360195c855b7452a5b53a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "942ce9608b06c88dd6d5",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ecf243170dbc43f5bfac",
+    "url": "/static/js/2.1ee98d3d.chunk.js"
+  },
+  {
+    "revision": "d38b49652b332ece4af6bb83fdfb8a30",
+    "url": "/static/js/2.1ee98d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942ce9608b06c88dd6d5",
+    "url": "/static/js/main.52428dc9.chunk.js"
+  },
+  {
+    "revision": "84539fc77b8eb9ab5b10",
+    "url": "/static/js/runtime-main.8c90acc2.js"
+  }
+]);
