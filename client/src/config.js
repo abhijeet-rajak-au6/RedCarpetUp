1cce74d6127122dabfcf05f177ff3fb1e75f0860
Abhijeet Rajak <rajakabhijeet6@gmail.com>
2020-12-02 00:28:19 +0530

    Done

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const keys = {
-	BASE_URL: "https://user-assignment.herokuapp.com",
+	BASE_URL: "https://user-assignment.herokuapp.com/api/v1",
 	BASE_URL_LOCAL: "http://localhost:1234/api/v1",
 };
 
